81bcd05492a1d9581e1078d45b8a1a6f9527e8b2
merveiri <123325645+merveiri@users.noreply.github.com>
2023-09-06 21:41:39 +0300

    Add project files.

---
+++ UnitTestsCourse/Tests/WorldsDumbestFunctionTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsCourse.Tests
+{
+    public static class WorldsDumbestFunctionTests
+    {
+        //Naming Convention - ClassName_MethodName_ExepctedResult
+        public static void WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnString()
+        {
+            try
+            {
+                //Arrange - Go get your variables, whatever you need, you classes, go get functions
+                int num = 0;
+
+                string info = "test";
+                
+                WorldsDumbestFunction worldsDumbest = new WorldsDumbestFunction();
+
+                Console.WriteLine("ekrana test data bilgisini yaz: "+worldsDumbest.testData(info));
+                
+                //Act - Execute this function
+                string result = worldsDumbest.ReturnsPikachuIfZero(num);
+                // Console.WriteLine("result: " + result);
+
+                //Assert - Whatever ever is returned is it what you want
+                if(result == "PIKACHU!")
+                {
+                    Console.WriteLine("PASSED: WorldsDumbestFunction.ReturnsPikachuIfZero_ReturnString");
+                }
+                else
+                {
+                    Console.WriteLine("FAILED: WorldsDumbestFunction.ReturnsPikachuIfZero_ReturnString");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ UnitTestsCourse/WorldsDumbestFunction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsCourse
+{
+    public class WorldsDumbestFunction
+    {
+        public string ReturnsPikachuIfZero(int num)
+        {
+            if(num == 0)
+            {
+                return "PIKACHU!";
+            }
+            else
+            {
+                return "Squirtle";
+            }
+        }
+
+        public string testData(string info)
+        {
+            if(info == "test")
+            {
+                return "doğru data: " + info;
+            } 
+            else if(info == "deneme")
+            {
+                return "hatalı data";
+            } else
+            {
+                return "Ben buradayım :)";
+            }
+           
+          
+        }
+    }
+}
